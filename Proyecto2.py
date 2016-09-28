522ac46f7ed17e70fae1299a1a87b0d7173c75db
JeanCarloM <toysiito@gmail.com>
2016-09-28 04:38:31 -0500

    Update Proyecto2.py

--- Proyecto2.py
+++ Proyecto2.py
@@ -7,3 +7,15 @@ def Fact(rec=1):
 
 print "Bienvenido a tu calculadora de Factoriales"
 print "Ingresa el numero a calcular"
+def Factorial(n):
+    if (n==0 or n==1):
+        return 1
+    else:
+        resultado = 0 #¿No se si esto sobra?
+        resultado = n * Factorial(n-1)
+        return resultado
+        print (resultado)
+fac = Factorial(3)
+print (fac)
+#Aun no entiendo bien como pedir el numero que se desea calcular y asginarselo a fac o a n, en ese si calcula el de 3, pero no se 
+#como asignar el numero del teclado a la variable :s
